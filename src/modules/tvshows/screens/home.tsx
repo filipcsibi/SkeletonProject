069bd31172edd2a4274cc79c2fb43024d8e0519e
Filip Csibi <filipcsibi@Filips-Mac-mini.local>
2023-05-04 19:51:11 +0300

    done

--- src/modules/tvshows/screens/home.tsx
+++ src/modules/tvshows/screens/home.tsx
@@ -3,7 +3,27 @@ import {HomeTabRoutes} from '../../../navigation/routes/hometab_routes';
 import {AuthState, useAuthStore} from '../../auth/store/useAuthStore';
 import {useState} from 'react';
 import Modal from 'react-native-modal';
+//https://6453db49e9ac46cedf31a3d0.mockapi.io/tvshows
 
+/*
+
+queryParams: search,page,field_name,
+
+    ?search=string for search
+    &page=number&limit=10 for pagination
+    HOME:
+    page with lista of tvshows paginate, search bar by name of show,
+    filter button langa search 
+    modal cu 2 filtre genre si durata scrollview orizontal cu cateva gen 10 nu toate
+    cand dam pe un ecran, ecran de details,pe ecran de details partea de favorites 
+    FAVORITES:
+    fara search si filter,o sa vina din lista noastra cele favorite,vin din global store nu din api deci pt fiecare user o sa avem favorite gen stocam tot obiectu la user
+    la lista de favorite buton pe card cu remove from favorites gen inima dinaia,si sa nu ne putem duce pe details cand apasam,
+    Profile:
+    ce avem pana acum plus interese
+    pe edit profile add intrese,cand le adaugam sa le vedem sub tot in edit gen si si pe profile screenr
+
+*/
 export const Home = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const Filter = () => {
